4e7253bca4563aa41d4a0921844e52ccebb6022e
Andy Bailey <neosonic@gmail.com>
2017-04-01 15:58:21 -0400

    Update Assembly Information
    Updated assembly information to reflect change of name (while retaining
    drop in compatibility)

--- libamiibo/Properties/AssemblyInfo.cs
+++ libamiibo/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("libamiibo")]
+[assembly: AssemblyTitle("libamiibo.noimage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("libamiibo")]
+[assembly: AssemblyProduct("libamiibo.noimage")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
